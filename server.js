4d14914dc08c864aecac990472489494a08c9e47
Dylan Hoffman <jkioicefire.gmail.com>
2021-09-03 15:13:44 -0500

    corrected server.js file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 //     res.send('Hi there');
 // })
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 app.use('/api/', require('./routes/hello'));
 
@@ -18,7 +18,7 @@ app.get('/test', (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
