837ecbae363578b4bf4918306d9dc46acf5ac51d
Dylan Hoffman <jkioicefire.gmail.com>
2021-09-03 14:52:08 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 const app = express();
 
@@ -6,11 +7,18 @@ const app = express();
 // app.get('/', (req, res) => {
 //     res.send('Hi there');
 // })
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.use('/api/', require('./routes/hello'));
 
 app.get('/test', (req, res) => {
     res.send('test!');
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
