1b1d073a1c82787c315f250a5826ac8e093ad889
Dylan Hoffman <jkioicefire.gmail.com>
2021-09-04 00:36:27 -0500

    troubleshooting nodejs backend

--- controllers/test.js
+++ controllers/test.js
@@ -1,6 +1,7 @@
 exports.test = (req, res) => {
-    res.json({
-        "welcome":["Thank you !"]
-    });
+    // res.json({
+    //     "welcome":["Thank you !"]
+    // });
+    res.sendFile('./test.html');
     console.log("hello from test.js controller");
 }
